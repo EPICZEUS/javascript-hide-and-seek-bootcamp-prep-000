37076aeb00c336a19c49722c39558efa739f7bbe
Sam B <s77broz@aol.com>
2018-08-31 20:44:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(s) {
+  //console.log("First Selector", document.querySelector(s));
+  return document.querySelector(s);
+}
 
+function nestedTarget() {
+  //console.log("Nested Target", document.querySelector("#nested .target"));
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  //console.log("Increase Rank By");
+  const rlists = document.querySelectorAll(".ranked-list");
+  for (let i = 0; i < rlists.length; i++) {
+    const list = rlists[i].querySelectorAll("li");
+    for (let j = 0; j < list.length; j++) {
+      list[j].innerHTML = parseInt(list[j].innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild() {
+  const deep = document.querySelector("#grand-node");
+  let deepest = deep.querySelector("div"), n = 1;
+  do {
+    deepest = deepest.querySelector("div");
+    console.log(deepest, n++);
+  } while (deepest.innerHTML.includes("<div>")) 
+  return deepest;
+}
