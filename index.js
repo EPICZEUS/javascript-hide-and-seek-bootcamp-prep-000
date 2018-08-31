1fdc21f32777b3d10d082bc8f16f9bc676a12db4
Sam B <s77broz@aol.com>
2018-08-31 19:40:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,7 @@ function nestedTarget() {
   return document.querySelector("#nested .target");
 }
 
-function increaseRankBy(n) {
-  const rlists = document.querySelectorAll(".ranked-list");
-  for (let i = 0; i < rlists.length; i++) {
-    const list = rlists[i].querySelectorAll("li");
-    for (let j = 0; j < list.length; j++) {
-      list[j].innerHTML = parseInt(list[j].innerHTML) + 1;
-    }
-  }
-}
+
 
 function deepestChild() {
   const deep = document.querySelector("#grand-node");
