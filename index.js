c6e9a85a925fffd9287e62db0f1e253c2be2b68f
Sam B <s77broz@aol.com>
2018-08-31 20:36:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function getFirstSelector(s) {
-  console.log("First Selector", document.querySelector(s));
+  //console.log("First Selector", document.querySelector(s));
   return document.querySelector(s);
 }
 
 function nestedTarget() {
-  console.log("Nested Target", document.querySelector("#nested .target"));
+  //console.log("Nested Target", document.querySelector("#nested .target"));
   return document.querySelector("#nested .target");
 }
 
 function increaseRankBy(n) {
-  console.log("Increase Rank By");
+  //console.log("Increase Rank By");
   const rlists = document.querySelectorAll(".ranked-list");
   for (let i = 0; i < rlists.length; i++) {
     const list = rlists[i].querySelectorAll("li");
