6f3116bc6e91c32757117cb683ff0720e0f26d80
Sam B <s77broz@aol.com>
2018-08-31 20:36:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function increaseRankBy(n) {
 function deepestChild() {
   const deep = document.querySelector("#grand-node");
   let deepest = deep.querySelector("div");
-  while (!/^<.+?>.*?<.+?>$/.test(deepest.innerHTML)) {
+  while (!/^<.+?>.*?<.+?>$/s.test(deepest.innerHTML)) {
     deepest = deepest.querySelector("div");
     console.log(deepest);
   }
