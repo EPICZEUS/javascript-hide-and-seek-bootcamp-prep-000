849c6a3a1fbdd5b95e71a603f2c6fc785eb34b8a
Sam B <s77broz@aol.com>
2018-08-31 19:41:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ function nestedTarget() {
   return document.querySelector("#nested .target");
 }
 
-
+function increaseRankBy(n) {
+  const rlists = document.querySelectorAll(".ranked-list");
+  for (let i = 0; i < rlists.length; i++) {
+    const list = rlists[i].querySelectorAll("li");
+    for (let j = 0; j < list.length; j++) {
+      list[j].innerHTML = parseInt(list[j].innerHTML) + n;
+    }
+  }
+}
 
 function deepestChild() {
   const deep = document.querySelector("#grand-node");
