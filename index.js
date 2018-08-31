098705540aa8962910d8e9c4d969b658b2cd1e08
Sam B <s77broz@aol.com>
2018-08-31 19:19:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function getFirstSelector(s) {
+  return document.querySelector(s);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  const rlists = document.querySelectorAll(".ranked-list");
+  for (let i = 0; i < rlists.length; i++) {
+    const list = rlists[i].querySelectorAll("li");
+    for (let j = 0; j < list.length; j++) {
+      list[j].innerHTML = parseInt(list[j].innerHTML) + 1;
+    }
+  }
+}
